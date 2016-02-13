58297659708febe3b21ab61ff2d6fd15921dc5c8
Wesley <wes@lawn-128-61-46-28.lawn.gatech.edu>
2016-02-12 22:51:49 -0500

    Initial Commit

---
+++ ChangeReturn/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ChangeReturn
+//
+//  Created by Wesley on 2/12/16.
+//  Copyright © 2016 Wesley. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
